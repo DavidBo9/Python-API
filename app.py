31823249f6d9ec740f2e67e150498095d75dc0f1
David Bojalil <194228@iberopuebla.mx>
2024-04-01 18:32:17 -0600

    Added env

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -43,6 +47,23 @@ def get_task(task_id):
     if task is None:
         abort(404)
     return jsonify(task.to_dict())
+
+@app.route('/tasks_update/<int:task_id>', methods=['GET'])
+def update_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        abort(404)
+    task.status = not task.status
+    return jsonify(task.to_dict())
+
+@app.route('/tasks_delete/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        abort(404)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({'status':True}), 201
 
 if __name__ == '__main__':
     with app.app_context():
